d75af1ec741ff963f2964ffb9199833104fe192d
Ievgen Umanets <ievgen@umanets.gmail.com>
2017-04-05 23:35:45 +0300

    server updated

--- server.js
+++ server.js
@@ -1,48 +1,56 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const port = process.env.port || 3000;
+
+const port = process.env.PORT || 3000;
 var app = express();
 
-app.set('viewengine', 'hbs');
-hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
 
-app.use((req, res, next) => {
-    const now = new Date().toDateString();
-    const log = `time :${now} method:${req.method} url:${req.url}`;
-    fs.appendFile('server.log', log + '\n', err => {
-        if (err) {
-            console.log('Unable to append to server log')
-        }
-    });
-    console.log(log);
-    next();
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
 });
-// app.use((req, res, next) => {
-//     res.render('maintance.hbs')
-// });
-app.use(express.static(__dirname + '/public'));
 
-app.get('/', (request, response) => {
-    // response.send('<h1> Hello Express </h1>');
-    response.render('home.hbs', {
-        name: 'Ievgen',
-        welcomeMsg: 'Android developer',
-        pageTitle: "Home Page",
-        currentYear: new Date().getFullYear()
-
-    })
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
+    });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: "About Page",
-    })
+        pageTitle: 'About Page'
+    });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
 });
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+    console.log(`Server is up on port ${port}`);
 });
