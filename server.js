bb3b8de5695eb1b74222b7718e94eca047ebe80b
Ievgen Umanets <ievgen@umanets.gmail.com>
2017-04-06 10:27:55 +0300

    server js port changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORTc || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -46,7 +46,9 @@ app.get('/bad', (req, res) => {
         errorMessage: 'Unable to handle request'
     });
 });
-
+server.connection({
+    port: process.env.PORT || 3000
+});
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
